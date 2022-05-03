12fbd1768d387a406345b03c871884130a8cfbcb
Luis Jimenez <luisrrjjcc@gmail.com>
2022-05-03 17:41:14 -0600

    Carousel agregado

--- assets/js/workshops.js
+++ assets/js/workshops.js
@@ -7,7 +7,10 @@ var workshops = [
         duration : '1 día',
         contact : 'Elisa Ejemplo',
         image : 'vr.png',
-        syllabus : ["Módulo 1: Conociendo Unity", "Módulo 2: Agregando Vuforia al proyecto", "Módulo 3: Importando un modelo 3D", "Módulo 4: Generando la aplicación"]
+        syllabus : ["Módulo 1: Conociendo Unity", "Módulo 2: Agregando Vuforia al proyecto", "Módulo 3: Importando un modelo 3D", "Módulo 4: Generando la aplicación"],
+
+        imgs : ['https://picsum.photos/id/973/200/300', 'https://picsum.photos/id/974/200/300', 'https://picsum.photos/id/975/200/300', 'https://picsum.photos/id/976/200/300'],
+        captions : ['Módulo 1', 'Módulo 2', 'Módulo 3', 'Módulo 4']
     },
     {
         name : 'Arduino',
