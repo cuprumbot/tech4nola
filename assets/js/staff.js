ce3e289eb7a7d4dbcea137e078716715fef7abe2
Luis Jimenez <luisrrjjcc@gmail.com>
2022-07-27 19:57:16 -0600

    logos, staff, workshops

--- assets/js/staff.js
+++ assets/js/staff.js
@@ -1,20 +1,38 @@
 var staff = [
     {
-        "name": "Elisa Ejemplo",
-        "short": "Elisa ha trabajado en múltiples proyectos.",
-        "information": "Elisa Ejemplo tiene años de experiencia, en los cuales ha trabajado en proyectos de distintas áreas.\n\nAqui hay bastante espacio para escribir la biografía e información completa.\n\nAlgunos intereses de Elisa son:\n- Robótica\n- Desarrollo web",
-        "image": "default-female.png",
-        "nationality": "guatemala",
-        "company": "Empresa Ejemplo",
-        "email": "elisa@ejemplo.com"
-    },
-    {
-        "name": "Esteban Ejemplo",
-        "short": "Esteban ha trabajado en el área de educación.",
-        "information": "Biografía larga va aquí.",
+        "name": "Adrián Catalán",
+        "short": "Co-organizer of several meetups in Guatemala, TensorFlow Certified Developer, Google for Startups Mentor, Google Developer Expert, Director of Innovation Lab at Galileo University, mountaineering enthusiast, stand-up comedian, educator at heart.",
+        "information": "Adrian has been part of the software industry for 17+ years as full stack, mobile and machine learning engineer. Past co-organizer of several meetups in Guatemala (GDG Cloud, GDG, Facebook DevC, JS.GT and Nodebots). TensorFlow Certified Developer, Google For Startups Mentor, Design Sprint Master and Google Developer Expert for Android, IoT and Firebase. Consultant, coach and speaker for a range of topics in LATAM, US, Europe & Asia. Currently he is the Innovation Lab Director at Galileo University.\n\nMountaineering enthusiast, has completed the Guatemalan 40 summits (\"Gran Montañista\" and \"Quetzal\" awards) and went to glacier climbing to Ecuador and Perú achieving several summits around 6000 meters. Stand-up, improv and clown comedian, he enjoys performing clown shows at nursing homes and sharing laughs with seniors. Passionate about diversity, inclusion and equity, he has organized several events focused on gender equality and mentor speakers from under represented groups. Educator at heart, has been teaching since he was 20 years old and worked on several initiatives in Latin America to empower people, from kids to industry professionals and successfully turn talent into skills.",
         "image": "default-male.png",
         "nationality": "guatemala",
-        "company": "Universidad Ejemplo",
-        "email": "esteban@uniejemplo.edu"
+        "company": "Universidad Galileo",
+        "email": "adriancatalan@galileo.edu"
+    },
+    {
+        "name": "Oscar Rodas",
+        "short": "",
+        "information": "",
+        "image": "default-male.png",
+        "nationality": "guatemala",
+        "company": "Universidad Galileo",
+        "email": ""
+    },
+    {
+        "name": "Vilma Saloj",
+        "short": "",
+        "information": "",
+        "image": "default-female.png",
+        "nationality": "guatemala",
+        "company": "",
+        "email": ""
+    },
+    {
+        "name": "Dulce García",
+        "short": "",
+        "information": "",
+        "image": "default-female.png",
+        "nationality": "guatemala",
+        "company": "Xumak",
+        "email": ""
     }
 ];
--- assets/js/workshops.js
+++ assets/js/workshops.js
@@ -1,24 +1,322 @@
 var workshops = [
     {
-        name : 'Realidad Virtual',
-        introduction : 'Crea tu primera aplicación de realidad virtual.',
-        information : ['En este taller aprenderemos a usar Unity y Vuforia.', 'El taller va orientado tanto a principiantes como a quienes ya tienen experiencia usando la herramienta. Crearemos una aplicación donde se detectará un patrón impreso en papel, y sobre este se proyectará un modelo 3D.', 'Necesitaras instalar Unity en tu computadora, así como cable para pasar la aplicación a tu teléfono.'],
-        tools : 'Unity, Vuforia',
-        duration : '1 día',
-        contact : 'Elisa Ejemplo',
-        image : 'vr.png',
-        syllabus : ["Módulo 1: Conociendo Unity", "Módulo 2: Agregando Vuforia al proyecto", "Módulo 3: Importando un modelo 3D", "Módulo 4: Generando la aplicación"],
+        name : 'Digital Literacy and Computational Thinking',
+        introduction : 'Basic concepts about computer usage and algorithms',
+        information : ['This introductory workshop will cover the basics about using computers.', 'The students will learn concepts about software and hardware, practice using productivity software and other cloud applications, and learn about some commonly used algorithms.'],
+        tools : 'Google Drive, printed materials, office supplies',
+        duration : '6 weeks',
+        image : 'problem.jpg',
 
+        completeSyllabus : [
+            {
+                name : 'M1 - Fundamentals',
+                topics : [
+                    'Computer basics and hardware', 
+                    'Where are computers used?', 
+                    'IT systems and how they work', 
+                    'World Wide Web and the Internet'
+                ]
+            },
+            {
+                name : 'M2 - Productivity applications',
+                topics : [
+                    'Productivity applications (Word processing, spreadsheets, presentations)',
+                    'Cloud services (Google Drive)',
+                    'Working with files and folder',
+                    'Collaboration and communication using the web'
+                ]
+            },
+            {
+                name : 'M3 - Tools',
+                topics : [
+                    'Virtual communities',
+                    'Proper internet searching techniques',
+                    'Programming languages',
+                    'Online tools (CodePen & TinkerCAD)'
+                ]
+            },
+            {
+                name : 'M4 - Computational thinking',
+                topics : [
+                    'Problem solving',
+                    'Handling abstractions',
+                    'Data collection',
+                    'Exploring algorithms'
+                ]
+            },
+            {
+                name : 'M5 - Algorithms and coding',
+                topics : [
+                    'Finding patterns',
+                    'Developing algorithms',
+                    'Coding structures (variables, data types)',
+                    'More coding (flow control, nesting)',
+                    'Debugging'
+                ]
+            },
+            {
+                name : 'M6 - More algorithms',
+                topics : [
+                    'Sorting algorithms',
+                    'Search algorithms',
+                    'Image representation'
+                ]
+            }
+        ]
+    },
+
+    {
+        name : 'Robotics Fundamentals',
+        introduction : 'Build your first robot',
+        information : ['This workshop will teach the students the basics of electronics and robotics.', 'The students will learn how to build and program a robot using Arduino, and then have a friendly competition between their robots.'],
+        tools : 'Arduino',
+        duration : '12 weeks',
+        image : 'arduino.png',
+
+        completeSyllabus : [
+            {
+                name : 'M1 - Engineering Fundamentals',
+                topics : [
+                    'What is engineering?',
+                    'Engineering languages',
+                    'Logical thinking',
+                    'Problem solving',
+                    'Creative thinking'
+                ]
+            },
+            {
+                name : 'M2 - Electricity Fundamentals',
+                topics : [
+                    'Electricity',
+                    'Voltage',
+                    'Current',
+                    'Power'
+                ]
+            },
+            {
+                name : 'M3 - Electronics Fundamentals',
+                topics : [
+                    'Electronic circuits',
+                    'Integrated circuits',
+                    'Transistors',
+                    'Resistors',
+                    'Capacitors',
+                    'Inductors'
+                ]
+            },
+            {
+                name : 'M4 - CAD & 3D Printing',
+                topics : [
+                    'Basic geometry',
+                    'Design system',
+                    'Software introduction',
+                    '3D design',
+                    'Combination of figures',
+                    'Export and print'
+                ]
+            },
+            {
+                name : 'M5 - Introduction to programming',
+                topics : [
+                    'Introduction to programming',
+                    'Flowchart',
+                    'Logical sequences',
+                    'Block programming',
+                    'Variables',
+                    'Cycles',
+                    'Conditionals'
+                ]
+            },
+            {
+                name : 'M6 - Advanced programming',
+                topics : [
+                    'Introduction to C++',
+                    'Introduction to Arduino IDE',
+                    'Arduino and hardware',
+                    'I/O management',
+                    'Use of actuators',
+                    'Use of transducers and sensors'
+                ]
+            },
+            {
+                name : 'M7 - Robotics',
+                topics : [
+                    'Introduction to robotics',
+                    'Robotics fundamentals',
+                    'Use of DC motors',
+                    'Use of servo motors',
+                    'Use of stepper motors'
+                ]
+            },
+            {
+                name : 'M8 - Prepare to compete',
+                topics : [
+                    'Bootcamp',
+                    'Build your robot',
+                    'Program your robot'
+                ]
+            },
+            {
+                name : 'M9 - Robotics competition',
+                topics : [
+                    'Competition',
+                    'Line-following rover',
+                    'Autonomous rover',
+                    'Light-following rover'
+                ]
+            },
+            {
+                name : 'M10 - Electronic applications',
+                topics : [
+                    'Other electronic kits',
+                    'Show students other projects on different platforms',
+                    'Inspire students to build their own electronic solution'
+                ]
+            },
+            {
+                name : 'M11 - Creating your own electronic solution',
+                topics : [
+                    'Bootcamp',
+                    'Build your electronic solution',
+                    'Program your electronic solution'
+                ]
+            },
+            {
+                name : 'M12 - Pitch your own electronic solution',
+                topics : [
+                    'Competition',
+                    'Elevator pitch',
+                    'Solve a real proglem in their communities or environment using technology'
+                ]
+            }
+        ]
+    },
+
+    {
+        name : 'Front-end web development with React',
+        introduction : 'Create a beautiful website using the React framework',
+        information : ['This workshop will teach the students how to create their first website.', 'The students will learn how to use HTML, CSS and JS to make websites. Then, the React framework will help them to simplify the process.'],
+        tools : 'React',
+        duration : '12 weeks',
+        image : 'react.jpg',
+
+        completeSyllabus : [
+            {
+                name : 'M1 - HTML, CSS & JavaScript',
+                topics : [
+                    'HTML tags',
+                    'Styling with CSS',
+                    'Box model',
+                    'Basic JS syntax',
+                    'JS arrays and functions'
+                ]
+            },
+            {
+                name : 'M2 - More JavaScript and Github',
+                topics : [
+                    'Working with the DOM',
+                    'Getting elements',
+                    'Modifying elements',
+                    'Version control software',
+                    'Github',
+                    'Branching and merging',
+                    'Collaboration workflow'
+                ]
+            },
+            {
+                name : 'M3 - Even more JavaScript',
+                topics : [
+                    'High-order functions',
+                    'Classes and objects',
+                    'JSON',
+                    'Map and filter',
+                    'Aynchronous JavaScript',
+                    'Promises',
+                    'Async / await'
+                ]
+            },
+            {
+                name : 'CSS Frameworks',
+                topics : [
+                    'CSS frameworks',
+                    'Material',
+                    'Responsive design',
+                    'Basic components',
+                    'Forms'
+                ]
+            },
+            {
+                name : 'M5 - Working with APIs',
+                topics : [
+                    'REST APIs',
+                    'Connecting with an external API',
+                    'Fetching data',
+                    'Parsing data'
+                ]
+            },
+            {
+                name : 'M6 - React basics',
+                topics : [
+                    'JSX syntax',
+                    'Thinking in React',
+                    'Describing the UI'
+                ]
+            },
+            {
+                name : 'M7 - Components and props',
+                topics : [
+                    'Designing components',
+                    'Component lifecycle',
+                    'Passing props',
+                    'Conditional rendering',
+                    'Rendering lists'
+                ]
+            },
+            {
+                name : 'M8 - Handling state',
+                topics : [
+                    'Adding interactivity',
+                    'Responding to events',
+                    'Updating state'
+                ]
+            },
+            {
+                name : 'M9 - State management',
+                topics : [
+                    'Reacting to input',
+                    'Designing structure',
+                    'Sharing between components',
+                    'Preserving and resetting',
+                    'Working with a reducer'
+                ]
+            },
+            {
+                name : 'M10 - Routing',
+                topics : [
+                    'How routes works',
+                    'Adding routes to your web app'
+                ]
+            },
+            {
+                name : 'M11 - Authentication',
+                topics : [
+                    'Auth basics',
+                    'Working with an auth provider',
+                    'Integrating with your web app'
+                ]
+            },
+            {
+                name : 'M12 - Deployment',
+                topics : [
+                    'Hosting',
+                    'Cloud providers'
+                ]
+            }
+        ]
+    }
+
+    /*
         imgs : ['https://picsum.photos/id/973/200/300', 'https://picsum.photos/id/974/200/300', 'https://picsum.photos/id/975/200/300', 'https://picsum.photos/id/976/200/300'],
         captions : ['Módulo 1', 'Módulo 2', 'Módulo 3', 'Módulo 4']
-    },
-    {
-        name : 'Arduino',
-        introduction : 'Construye tu primer robot usando Arduino.',
-        information : ['En este taller aprenderemos lo básico de electrónica y robótica gracias a Arduino.', 'En el taller utilizaremos Arduino para controlar un robot que puede avanzar y detenerse según su entorno. Usaremos sensores ultrasónicos para detectar objetos y evitar colisiones.'],
-        tools : 'Arduino',
-        duration : '4 horas',
-        contact : 'Esteban Ejemplo',
-        image : 'arduino.png'
-    }
+    */
 ];
