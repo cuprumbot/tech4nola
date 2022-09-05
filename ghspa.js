4701a1bab91694f3bcad711f1b4110345d4a7e0d
Luis Jimenez <luisrrjjcc@gmail.com>
2022-09-05 14:27:57 -0600

    mobile

--- ghspa.js
+++ ghspa.js
@@ -25,7 +25,7 @@
    /* redirect all 404 trafic to index.html */
    function redirect() {
      //var hash = '#' + l.pathname[1];
-     var hash = '#' + l.pathname.split('/')[2];
+     var hash = '#' + l.pathname.split('/')[2][0];
      l.replace(l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') + repo + '/?' +
               (l.pathname ? 'p=' + l.pathname.replace(/&/g, '~and~').replace(repo, '') : '') +
               (l.search ? '&q=' + l.search.slice(1).replace(/&/g, '~and~') : '') +
